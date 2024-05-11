0245df675d38107c9804d2973074a0a2edb65cdb
winartinurutami <winarti.nur.ft20@mail.umy.ac.id>
2024-05-11 11:19:07 +0700

    fungsi peek atau top pada tumpukan (stack). Fungsi ini digunakan untuk mengambil nilai dari elemen teratas tumpukan tanpa menghapusnya.

--- stackusingsinly/stackusingsinly.cpp
+++ stackusingsinly/stackusingsinly.cpp
@@ -39,3 +39,8 @@ public:
 		}
 		cout << "Popped value: " << top->data << endl;
 		top = top->next; // update the top pointer to the next node
+	}
+	//peek/top operation: retrieve the value of the topmost element without removing it
+	void peek() {
+		if (top == NULL) {
+			cout << "List is empty." << endl;
